1520e200ee610c147e7eecf2f29ad3f0ecfe5cde
shuvo-art <shuvo83qn@gmail.com>
2024-01-22 01:52:42 +0600

    module concept

--- app.js
+++ app.js
@@ -5,7 +5,16 @@
 // module - info about current module (file)
 // process - info about env where the program is being executed
 
-console.log(__dirname)
-setInterval(() => {
-    console.log('hello world')
-}, 1000)
+//CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const john = 'john'
+const peter = 'peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name} !!!`)
+}
+
+sayHi('susan')
+sayHi(john)
+sayHi(peter)
