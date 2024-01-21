6133b44ab5a61db8390daf6452d0d51c8f2d5a74
shuvo-art <shuvo83qn@gmail.com>
2024-01-22 01:22:28 +0600

    first test of node app

---
+++ 01-intro.js
@@ -0,0 +1,17 @@
+// GLOBALS - NO WINDOW !!!
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+const num = 12;
+
+if (num < 10) {
+    console.log('small number')
+} else {
+    console.log('large number')
+}
+
+console.log(`hey it's my first node app!!!`)
+
