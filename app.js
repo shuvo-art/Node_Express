af0b45a478ec6144c9d97cb953bd988b4a60bb9b
shuvo-art <shuvo83qn@gmail.com>
2024-01-23 14:11:57 +0600

    package.json file creation ignoring node_modules

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// npm - global command, comes with node
+// npm -global command, comes with node
 // npm --version
 
 // local dependency - use it only in this particular project
@@ -13,9 +13,9 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
-const _ = require('lodash')
+const _ = require('lodash');
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
 
-console.log(newItems)
+console.log(newItems);
