41877950a1b47357397d493f5a5b7330041b20c1
shuvo-art <shuvo83qn@gmail.com>
2024-01-24 21:21:18 +0600

    Easy Promises & Read Write

--- app.js
+++ app.js
@@ -15,17 +15,13 @@
 
 // npm i nodemon -D (dev dependency)
 
-const { readFile,writeFile } = require('fs');
-const util = require('util');
-
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const { readFile,writeFile } = require('fs').promises;
 
 const start = async () => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
-        await writeFilePromise(
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile(
             './content/result-mind-grenade.txt',
             `THIS IS AWESOME : ${first} ${second}`,
             { flag: 'a' }
