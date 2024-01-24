5c4ef9402b5299a1ff2b4249064226a81dc33199
shuvo-art <shuvo83qn@gmail.com>
2024-01-24 21:18:32 +0600

    Promise & Read Write

--- app.js
+++ app.js
@@ -15,3 +15,54 @@
 
 // npm i nodemon -D (dev dependency)
 
+const { readFile,writeFile } = require('fs');
+const util = require('util');
+
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+const start = async () => {
+    try {
+        const first = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        await writeFilePromise(
+            './content/result-mind-grenade.txt',
+            `THIS IS AWESOME : ${first} ${second}`,
+            { flag: 'a' }
+        )
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+/* const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf8', (err, result) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            resolve(result)
+        });  
+    });
+} */
+
+    
+/* getText('./content/first.txt')
+    .then((result) => console.log(result))
+    .catch((err) => console.log(err)) */
+            
+/* const getText = () => {
+    readFile('./content/first.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(result)
+    });
+} */
+
+//getText();
