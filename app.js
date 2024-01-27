75f87739f455954ddcf093ecfc121bc6e8b4ee00
shuvo-art <shuvo83qn@gmail.com>
2024-01-28 01:00:51 +0600

    Web app push own server

---
+++ app.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const { readFileSync } = require('fs')
+
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeLogo = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    //console.log(req.method)
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    else if (url === '/styles.css') {
+        res.writeHead(200, {'content-type': 'text/css'})
+        res.write(homeStyles)
+        res.end()
+    }
+    else if (url === '/logo.svg') {
+        res.writeHead(200, {'content-type': 'image/svg+xml'})
+        res.write(homeLogo)
+        res.end()
+    }
+    else if (url === '/browser-app.js') {
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    }
+    else if (url === '/about') {
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+    else {
+        res.writeHead(404, {'content-type': 'text/html'})
+        res.write('<h1>Page not found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000)
